46195722c6c2efddbc5e3a08a34a46188e265307
JitChowDhury <jitchowdhury8717@gmail.com>
2025-08-11 21:12:14 +0530

    Set Up Things For The Project
    -Add Reqd. Packages
    -Test Multiplayer

---
+++ Assets/Scripts/JoinServer.cs
@@ -0,0 +1,11 @@
+using Unity.Netcode;
+using UnityEngine;
+
+public class JoinServer : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void Join()
+    {
+        NetworkManager.Singleton.StartClient();
+    }
+}
