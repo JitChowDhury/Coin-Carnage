ec88d09adc8d13881d68d995ae76a6ae3c9ecdc8
JitChowDhury <jitchowdhury8717@gmail.com>
2025-08-20 10:36:39 +0530

    Network Authority

--- Assets/Scripts/ConnectionButtons.cs
+++ Assets/Scripts/ConnectionButtons.cs
@@ -1,11 +1,16 @@
 using Unity.Netcode;
 using UnityEngine;
 
-public class JoinServer : MonoBehaviour
+public class ConnectionButtons : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public void Join()
+    public void StartClient()
     {
         NetworkManager.Singleton.StartClient();
     }
+    public void StartHost()
+    {
+        NetworkManager.Singleton.StartHost();
+    }
+
 }
